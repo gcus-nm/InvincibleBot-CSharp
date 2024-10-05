966f8ce03f501105bf63d155837d251aa919fc0e
gcus-nm <gcusnm12@gmail.com>
2024-10-05 23:55:38 +0900

    feat: スラッシュコマンド対応版のコマンドたち

---
+++ Minecraft/MinecraftCommand.cs
@@ -0,0 +1,95 @@
+using Discord;
+using Discord.Commands;
+using Discord.Commands.Builders;
+using Discord.WebSocket;
+using InvincibleBot.Core;
+
+namespace InvincibleBot.Minecraft
+{   
+    public class MinecraftCommand : ModuleBase
+    {
+        protected override void OnModuleBuilding(CommandService commandService, ModuleBuilder builder)
+        {
+            base.OnModuleBuilding(commandService, builder);
+
+            DiscordBotClient.Instance.BotClient.Ready += async () => await Regist();
+            DiscordBotClient.Instance.BotClient.SlashCommandExecuted += OnSlashCommandExecuted;
+        }
+
+        /// <summary>
+        /// スラッシュコマンドの登録
+        /// </summary>
+        /// <returns></returns>
+        private async Task Regist()
+        {
+            SlashCommandBuilder builder = new SlashCommandBuilder()
+                .WithName("mine")
+                .WithDescription("Minecraft関連のコマンド")
+                .AddOption(new SlashCommandOptionBuilder()
+                    .WithName("start")
+                    .WithDescription("サーバーを起動します")
+                    .WithType(ApplicationCommandOptionType.SubCommand)
+                ).AddOption(new SlashCommandOptionBuilder()
+                    .WithName("stop")
+                    .WithDescription("サーバーを停止します")
+                    .WithType(ApplicationCommandOptionType.SubCommand)
+                );
+
+            try
+            {
+                var guild = DiscordBotClient.Instance.BotClient.GetGuild(951653786307395605);
+                await guild.CreateApplicationCommandAsync(builder.Build());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        /// <summary>
+        /// スラッシュコマンドが実行されたときの処理
+        /// </summary>
+        /// <param name="command">コマンドの内容</param>
+        /// <returns></returns>
+        private async Task OnSlashCommandExecuted(SocketSlashCommand command)
+        {
+            if (command.Data.Name != "mine")
+            {
+                return;
+            }
+
+            switch(command.Data.Options.First().Name)
+            {
+                case "start":
+                    await Start(command);
+                    break;
+                case "stop":
+                    await Stop(command);
+                    break;
+                default:
+                    await command.RespondAsync("コマンドを処理できませんでした。");
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// サーバーを起動する
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        private async Task Start(SocketSlashCommand command)
+        {
+            await command.RespondAsync("サーバーを起動します");
+        }
+
+        /// <summary>
+        /// サーバーを停止する
+        /// </summary>
+        /// <param name="command"></param>
+        /// <returns></returns>
+        private async Task Stop(SocketSlashCommand command)
+        {
+            await command.RespondAsync("サーバーを停止します");
+        }
+    }
+}
